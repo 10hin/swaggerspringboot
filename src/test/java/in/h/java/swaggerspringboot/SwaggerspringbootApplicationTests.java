8abffb1bdc107cf66b1224d08dea12c4c4946eed
10hin <10hin@outlook.jp>
2022-08-20 14:17:58 +0900

    add start.spring.io generated code

---
+++ src/test/java/in/h/java/swaggerspringboot/SwaggerspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.h.java.swaggerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
